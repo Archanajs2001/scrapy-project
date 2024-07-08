b584dc64004f4edac49a6545b95e1744848e4c5a
Archana <archanaganga585@gmail.com>
2024-07-08 21:27:12 +0530

    created spiders

--- agents/spiders/agentspider.py
+++ agents/spiders/agentspider.py
@@ -74,3 +74,105 @@ print(profile_links)
 
 
 
+#spider
+
+import scrapy
+import re
+
+class AgentsSpider(scrapy.Spider):
+    name = "agent"
+    allowed_domains = ['bhhsamb.com']
+    start_urls = ['https://www.bhhsamb.com/roster/Agents']
+
+    def parse(self, response):
+        # Use the links obtained from Playwright
+        global profile_links
+        for link in profile_links:
+            yield response.follow(link, callback=self.parse_agents)
+
+    def parse_agents(self, response):
+        name = response.xpath('//div[@class="site-global-container"]//p[@class="rng-agent-profile-contact-name"]/text()').get()
+        if name:
+            name = name.strip()
+
+        job_title = response.xpath('//div[@class="site-global-container"]//p[@class="rng-agent-profile-contact-name"]/span/text()').get()
+        if not job_title:
+            job_title = 'Realtor'
+            
+        image_url = response.xpath('//div[@class="site-global-container"]//article[@class="rng-agent-profile-main"]/img/@src').get()
+        if not image_url:
+            image_url = 'No Image'
+
+        address1 = response.xpath('//div[@class="site-global-container"]//li[@class="rng-agent-profile-contact-address"]//strong/text()[1]').get().strip()
+        address2 = response.xpath('//div[@class="site-global-container"]//li[@class="rng-agent-profile-contact-address"]//strong/following::text()').get().strip()
+
+        languages = response.xpath('//div[@class="site-global-container"]//p[@class="rng-agent-profile-languages"]/text()').getall()
+        
+        phone = response.xpath('//div[@class="site-global-container"]//li[@class="rng-agent-profile-contact-phone"]/a/@href').get()
+        facebook = response.xpath('//div[@class="site-global-container"]//li[@class="social-facebook"]/a/@href').get()
+        twitter = response.xpath('//div[@class="site-global-container"]//li[@class="social-twitter"]/a/@href').get()
+        linkedin = response.xpath('//div[@class="site-global-container"]//li[@class="social-linkedin"]/a/@href').get()
+        youtube = response.xpath('//div[@class="site-global-container"]//li[@class="social-youtube"]/a/@href').get()
+        pinterest = response.xpath('//div[@class="site-global-container"]//li[@class="social-pinterest"]/a/@href').get()
+        instagram = response.xpath('//div[@class="site-global-container"]//li[@class="social-instagram"]/a/@href').get()
+        website = response.xpath('//div[@class="site-global-container"]//li[@class="rng-agent-profile-contact-website"]/a/@href').get()
+        email = response.xpath('//div[@class="site-global-container"]//li[@class="rng-agent-profile-contact-email"]/a/@href').get()
+        description = response.xpath('//div[@class="site-global-container"]//article[@class="rng-agent-profile-content"]//text()').getall()
+        #response.xpath('//div[@class="site-global-container"]//article[@class="rng-agent-profile-content"]//p//text()').getall()    
+
+
+
+        social_accounts = {
+            'facebook': facebook,
+            'twitter': twitter,
+            'linkedin': linkedin,
+            'youtube': youtube,
+            'pinterest': pinterest,
+            'instagram': instagram
+        }
+
+        address = f"{address1}, {address2}"
+
+        if phone:
+            phone = phone.replace('tel:', '')
+
+         
+        
+        contact_details = {
+            'cell': phone,
+            # 'email': email,
+        }
+        
+        def clean_description(description):
+            # Regex pattern to match 'About' followed by 0 to 2 words
+            about_pattern = re.compile(r'\bAbout\b(\s+\w+){0,2}')
+
+            # Clean and concatenate the description into a single string
+            cleaned_description = ' '.join([
+                about_pattern.sub('', text.strip()).replace('More information about me.', '')
+                for text in description if text.strip()
+            ])
+
+            return cleaned_description.strip() or 'No description'
+    
+
+        # Clean the description
+        cleaned_description = clean_description(description)
+
+                
+
+        offices = address2.split(',')[0] + ' Office'
+
+    
+        
+        yield {
+            'name': name,
+            'job_title': job_title,
+            'image_url': image_url,
+            'address': address,
+            'contact_details': contact_details,
+            'social_accounts': social_accounts,
+            'offices': offices,
+            'languages': languages,
+            'description': cleaned_description
+        }
